be107a9318feea374d60e28266a89a1d9679521c
Alisha <jafrya04@gmail.com>
2023-03-31 18:43:14 -0400

    close to getting the image loaded

--- app/src/main/java/com/example/and101proj5randommovies/MainActivity.kt
+++ app/src/main/java/com/example/and101proj5randommovies/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.example.and101proj5randommovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+
 
 class MainActivity : AppCompatActivity() {
     var movieImageURL = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         var movieImage =  findViewById<ImageView>(R.id.movie_image)
         var movieButton = findViewById<Button>(R.id.movie_button)
@@ -25,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         getMovieImageURL()
         Log.d("movieImageURL", "movie image URL set")
 
-        //getNextImage(movieButton, movieImage)
+        getNextImage(movieButton, movieImage)
     }
 
     //figure out api https://imdb-api.com/api#Report-header
@@ -37,11 +41,15 @@ class MainActivity : AppCompatActivity() {
         val client = AsyncHttpClient()
 
         client["https://imdb-api.com/en/API/Posters/k_7o0ubhxp/tt1375666", object : JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
                 Log.d("Movie", "response successful")
-                //????
-                movieImageURL = json.jsonObject.getJSONArray("posters")
 
+                val posters = json.jsonObject.getJSONArray("posters")
+                if(posters.length()>0){
+                    movieImageURL = posters.getJSONObject(0).getString("")
+                } else{
+                    movieImageURL = ""
+                }
             }
 
             override fun onFailure(
@@ -56,7 +64,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /*private fun getNextImage(button: Button, imageView: ImageView) {
+    private fun getNextImage(button: Button, imageView: ImageView) {
         button.setOnClickListener {
             getMovieImageURL()
             Glide.with(this)
@@ -66,5 +74,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-     */
+
 }
